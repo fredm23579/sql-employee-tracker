7035125c5a42a49fccf86499256d1c4ceb5a1c77
fredm23579 <motta@g.ucla.edu>
2024-05-04 20:56:49 -0700

    "updated queries.js, connection.js, and schema.sql"

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary DECIMAL(10, 2),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
