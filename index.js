ef8b5fcfd7a54da0787c206435691397b0d09075
fredm23579 <motta@g.ucla.edu>
2024-05-04 20:37:32 -0700

    "added initial code/comments to index.js"

---
+++ index.js
@@ -0,0 +1,13 @@
+// Your main application code goes here
+
+// Example usage:
+// const inquirer = require('inquirer');
+// const { getAllDepartments } = require('./lib/queries');
+
+// async function main() {
+//     const departments = await getAllDepartments();
+//     console.log(departments);
+//     // Implement the rest of your application logic here
+// }
+
+// main();
