2632d9c0e600534cf05dbd29a9d84d8d9ff57b61
fredm23579 <motta@g.ucla.edu>
2024-05-04 21:12:34 -0700

    "updated schema.sql, package.json, added .env file and .gitignore file"

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,9 @@
 -- Create the department table
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
